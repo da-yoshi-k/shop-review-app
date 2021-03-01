9e0adb0e67924ffd21ead725ff06bc656123bec1
da-yoshi-k <hey234234s@gmail.com>
2021-03-02 08:52:02 +0900

    StackNavigatorの実装

--- App.tsx
+++ App.tsx
@@ -1,48 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, FlatList, View, SafeAreaView, LogBox } from 'react-native';
-/* lib */
-import { getShops } from './src/lib/firebase';
-/* components */
-import { ShopReviewItem } from './src/components/ShopReviewItem';
-/* types */
-import { Shop } from './src/types/Shop';
+import { AppNavigator } from './src/navigation/AppNavigator';
+import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Setting a timer']);
 
 export default function App() {
-  const [shops, setShops] = useState<Shop[]>([]);
-
-  useEffect(() => {
-    getFirebaseItems();
-  }, []);
-
-  const getFirebaseItems = async () => {
-    const searchedShops = await getShops();
-    setShops(searchedShops);
-  };
-
-  const shopItems = shops.map((shop, index) => (
-    <ShopReviewItem shop={shop} key={index.toString()} />
-  ));
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={shops}
-        renderItem={({ item }: { item: Shop }) => (
-          <ShopReviewItem shop={item} />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-        numColumns={2}
-      />
-    </SafeAreaView>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
