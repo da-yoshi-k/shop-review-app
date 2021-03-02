ca370413083ffefe285baf833cfccc4d8305192b
da-yoshi-k <hey234234s@gmail.com>
2021-03-03 00:57:27 +0900

    画面遷移の実装

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 /* navigator */
-import { HomeStackNavigator } from './HomeStackNavigator';
+import { MainTabNavigator } from './MainTabNavigator';
+/* screens */
+import { AuthScreen } from '../screens/AuthScreen';
 
 export const AppNavigator = () => {
+  const user = { id: 123 };
+
   return (
     <NavigationContainer>
-      <HomeStackNavigator />
+      {!user ? <AuthScreen /> : <MainTabNavigator />}
     </NavigationContainer>
   );
 };
--- src/navigation/HomeStackNavigator.tsx
+++ src/navigation/HomeStackNavigator.tsx
@@ -3,12 +3,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 /* screens */
 import { HomeScreen } from '../screens/HomeScreen';
 import { ShopScreen } from '../screens/ShopScreen';
+import { CreateReviewScreen } from '../screens/CreateReviewScreen';
 /* types */
 import { RootStackParamList } from '../types/navigation';
 
 const Stack = createStackNavigator<RootStackParamList>();
+const RootStack = createStackNavigator<RootStackParamList>();
 
-export const HomeStackNavigator = () => {
+const MainStack = () => {
   return (
     <Stack.Navigator
       screenOptions={{
@@ -22,5 +24,20 @@ export const HomeStackNavigator = () => {
       />
       <Stack.Screen name="Shop" component={ShopScreen} />
     </Stack.Navigator>
+  );
+};
+
+export const HomeStackNavigator = () => {
+  return (
+    <RootStack.Navigator mode="modal">
+      <RootStack.Screen
+        name="Main"
+        component={MainStack}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <RootStack.Screen name="CreateReview" component={CreateReviewScreen} />
+    </RootStack.Navigator>
   );
 };
